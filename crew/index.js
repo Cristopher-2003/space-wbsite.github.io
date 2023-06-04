a2fc308a2ef7658129b948936da55a087b27faaf
Cristopher-2003 <110250316+Cristopher-2003@users.noreply.github.com>
2023-06-03 19:30:56 -0500

    commit

---
+++ crew/index.js
@@ -0,0 +1,38 @@
+function navegar() {
+    var pagina1crew = document.getElementById("pagina1");
+    var pagina2crew = document.getElementById("pagina2");
+    var pagina3crew = document.getElementById("pagina3");
+    var pagina4crew = document.getElementById("pagina4");
+
+  
+    if (pagina1crew.checked) {
+      window.location.href = "crew1.html";
+      pagina2crew.classList.remove("selected");
+    pagina3crew.classList.remove("selected");
+    pagina4crew.classList.remove("selected");
+    pagina1crew.classList.add("selected");
+
+
+    } else if (pagina2crew.checked) {
+      window.location.href = "crew2.html";
+      pagina2crew.classList.add("selected");
+    pagina3crew.classList.remove("selected");
+    pagina1crew.classList.remove("selected");
+    pagina4crew.classList.remove("selected");
+
+    } else if (pagina3crew.checked) {
+      window.location.href = "crew3.html";
+      pagina2crew.classList.remove("selected");
+      pagina1crew.classList.remove("selected");
+      pagina4crew.classList.remove("selected");
+    pagina3crew.classList.add("selected");
+    }
+    else if (pagina4crew.checked) {
+      window.location.href = "crew4.html";
+      pagina1crew.classList.remove("selected");
+      pagina2crew.classList.remove("selected");
+    pagina3crew.classList.remove("selected");
+    pagina4crew.classList.add("selected");
+    }
+  }
+  
