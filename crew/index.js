c21b308952a571ed2c6331afe7abde15ee2be86d
Cristopher-2003 <110250316+Cristopher-2003@users.noreply.github.com>
2023-06-06 13:11:35 -0500

    si claro que si

--- crew/index.js
+++ crew/index.js
@@ -1,10 +1,5 @@
 function navegar() {
-    var pagina1crew = document.getElementById("pagina1");
-    var pagina2crew = document.getElementById("pagina2");
-    var pagina3crew = document.getElementById("pagina3");
-    var pagina4crew = document.getElementById("pagina4");
-     
-    /*desktop*/ 
+    
     var pagina1crew = document.getElementById("pagina01");
     var pagina2crew = document.getElementById("pagina02");
     var pagina3crew = document.getElementById("pagina03");
@@ -39,6 +34,43 @@ function navegar() {
       pagina2crew.classList.remove("selected");
     pagina3crew.classList.remove("selected");
     pagina4crew.classList.add("selected");
+    }
+
+    /**para desktop */
+
+    var pagina1crewd = document.getElementById("pagina-uno");
+    var pagina2crewd = document.getElementById("pagina-dos");
+    var pagina3crewd = document.getElementById("pagina-tres");
+    var pagina4crewd = document.getElementById("pagina-cuatro");
+
+    if (pagina1crewd.checked){
+      window.location.href = "crew1.html";
+      pagina2crewd.classList.remove("selected");
+    pagina3crewd.classList.remove("selected");
+    pagina4crewd.classList.remove("selected");
+    pagina1crewd.classList.add("selected");
+
+
+    } else if (pagina2crewd.checked) {
+      window.location.href = "crew2.html";
+      pagina2crewd.classList.add("selected");
+    pagina3crewd.classList.remove("selected");
+    pagina1crewd.classList.remove("selected");
+    pagina4crewd.classList.remove("selected");
+
+    } else if (pagina3crewd.checked) {
+      window.location.href = "crew3.html";
+      pagina2crewd.classList.remove("selected");
+      pagina1crewd.classList.remove("selected");
+      pagina4crewd.classList.remove("selected");
+    pagina3crewd.classList.add("selected");
+    }
+    else if (pagina4crewd.checked) {
+      window.location.href = "crew4.html";
+      pagina1crewd.classList.remove("selected");
+      pagina2crewd.classList.remove("selected");
+    pagina3crewd.classList.remove("selected");
+    pagina4crewd.classList.add("selected");
     }
   }
   
